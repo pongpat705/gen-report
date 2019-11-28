6dc4842847e70b2beb321ca77b4dd8955f2ce90a
fhluky <fhluky@gmail.com>
2019-11-28 13:59:15 +0700

    Initial commit

---
+++ src/test/java/com/maoz/app/genreport/GenReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maoz.app.genreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
